a13af12184b5744495eefe9dc4e34c7cd5d3d7fc
tacodtripe <55043949+tacodtripe@users.noreply.github.com>
2021-11-12 11:29:28 -0700

    TDD: approach capitalize function must capitalize the first letter of the string

--- capitalizestring.js
+++ capitalizestring.js
@@ -2,6 +2,9 @@ function capitalize(str) {
     if(typeof str !== 'string'){
         throw new Error('must provide a string')
     }
+    let holder = str.substr(1)
+    let capitalized = str[0].toUpperCase()
+    return capitalized + holder
 }
 
 module.exports = capitalize
--- capitalizestring.test.js
+++ capitalizestring.test.js
@@ -3,3 +3,6 @@ const capitalize = require('./capitalizestring')
 test('the function gets a string as parameter', () => {
     expect(() => capitalize(132)).toThrow('must provide a string')
 })
+test('the function capitalize the first letter', () => {
+    expect(capitalize('whatever')).toBe('Whatever')
+})
